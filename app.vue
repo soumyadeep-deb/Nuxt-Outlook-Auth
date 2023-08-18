9dc69d9c3db89811285aa6a15ad8a157373bc289
soumyadeep-deb <oblique.n@gmail.com>
2023-08-18 20:14:33 +0530

    App.vue implementation

---
+++ app.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+const { status, signIn, signOut, data } = useSession()
+
+const loggedIn = computed(() => status.value === 'authenticated')
+
+async function handleSignIn() {
+  await signIn()
+}
+
+async function handleSignOut() {
+  await signOut()
+}
+</script>
+
+<template>
+  <div>
+    <div>
+      <div>
+        <div v-if="loggedIn">
+          <NuxtPage/>
+          <div>{{ data }}</div>
+        </div>
+
+      </div>      
+      <button v-if="loggedIn" @click="handleSignOut">Log Out</button>
+      <div v-else>
+        <h2>Please Log in with your Outlook Account</h2>
+        <button @click="handleSignIn">
+          Log In
+        </button>
+      </div>
+      
+    </div>
+  </div>
+</template>
