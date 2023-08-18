b53cba018e8143ab02c35f3d268915b3f72ca7df
soumyadeep-deb <oblique.n@gmail.com>
2023-08-18 20:15:19 +0530

    Made a few pages

---
+++ pages/home.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1>Home Page</h1>
+    <h1>You are logged in now!</h1>
+  </div>
+</template>
+
+<script>
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<script>
+</script>
+
+<template>
+  <div>
+    <h1>Welcome to NUXT auth project</h1>
+    <div>
+      <NuxtLink to="/home">
+        <button>
+          Go to Home Page
+        </button>
+      </NuxtLink>
+    </div>
+  </div>
+</template>
+
+
+<style scoped>
+
+</style>
